b9468e9dca90c5e85b68f35f439707f89644ddd7
imskanand <the.anand3099@gmail.com>
2022-06-13 22:56:12 +0530

    Initilized the CSES Problem Set Repo

---
+++ 0. Introductory-Problem/Weird_Algorithm.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+typedef long long ll;
+int main()
+{
+  ll n;
+  cin >> n;
+
+  vector<ll> v;
+  v.push_back(n);
+
+  while (n != 1)
+  {
+    if (n % 2 == 1)
+    {
+      n = (n * 3) + 1;
+    }
+    else
+    {
+      n /= 2;
+    }
+    v.push_back(n);
+  }
+
+  for (auto i : v)
+  {
+    cout << i << " ";
+  }
+  return 0;
+}
